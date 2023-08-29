9794e169cf55dc3155f564c91947da5c2994ffe3
rustandbone <dlehdgh14@naver.com>
2023-08-29 23:57:50 +0900

    tanstack 적용

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,21 @@ import { HelmetProvider } from 'react-helmet-async';
 import { Toaster } from 'react-hot-toast';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Suspense } from 'react';
+import Spinner from './components/Spinner';
+
+//쿼리 클라이언트 객체 생성
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // retry: 5, //기본값은 3
+      // staleTime : 1 * 1000 * 60 * 60 * 24 * 7; 
+      // cacheTime: 
+    }
+  }
+}); //모든 쿼리에 사용되는 기본 옵션
 
 function App() {
   return (
@@ -11,9 +26,16 @@ function App() {
       <HelmetProvider>
         <ThemeProvider>
           <AuthProvider>
-            <div className="App">
-              <RouterProvider router={router} />
-            </div>
+            {/* 쿼리 클라이언트를 앱에 공급 */}
+            <QueryClientProvider client={queryClient}>
+              <div className="App">
+                <Suspense fallback={<Spinner size={200} messeage="페이지 로딩 중..." />}>
+                  <RouterProvider router={router} />
+                </Suspense>
+              </div>
+              {/* tanstack 쿼리 개발 도구 */}
+              <ReactQueryDevtools />
+            </QueryClientProvider>
           </AuthProvider>
         </ThemeProvider>
       </HelmetProvider>
