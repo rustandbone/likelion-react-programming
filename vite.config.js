5f19c8e5f26b47882bebb951c46f5a05ce88d21d
rustandbone <dlehdgh14@naver.com>
2023-08-29 23:55:44 +0900

    배포 최적화(이미지, 코드)

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,60 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import { env } from 'node:process';
+import viteImagemin from '@vheemstra/vite-plugin-imagemin';
+import imageminGifSicle from 'imagemin-gifsicle';
+import imageminMozjpeg from 'imagemin-mozjpeg';
+import imageminPngQuant from 'imagemin-pngquant';
+import imageminSvgo from 'imagemin-svgo';
+import imageminWebp from 'imagemin-webp';
 
 const isDev = env.NODE_ENV === 'development';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // 청크(chunk) 파일 생성 플러그인 구성
+    splitVendorChunkPlugin(),
+    // 이미지 최적화 플러그인 구성
+    viteImagemin({
+      plugins: {
+        jpg: imageminMozjpeg(),
+        png: imageminPngQuant(),
+        gif: imageminGifSicle(),
+        svg: imageminSvgo(),
+      },
+      makeWebp: {
+        plugins: {
+          jpg: imageminWebp(),
+          png: imageminWebp(),
+        },
+      },
+    }),
+  ],
   css: {
     devSourcemap: true,
     modules: {
-      generateScopedName: isDev ? '[name]_[local]__[hash:base64:5]' : '[hash:base64:4]'
-    }
+      generateScopedName: isDev
+        ? '[name]_[local]__[hash:base64:5]'
+        : '[hash:base64:4]',
+    },
   },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          reactRouter: ['react-router-dom'],
+          animations: ['gsap', 'framer-motion'],
+        },
+      },
     },
   },
 });
