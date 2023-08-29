5f6517b538ba0fe41300e6a680eb95ade1c0120b
rustandbone <dlehdgh14@naver.com>
2023-08-29 23:59:48 +0900

    footer memo 적용

--- src/layout/FooterBar.jsx
+++ src/layout/FooterBar.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+import { useState, memo } from 'react';
 import { motion } from 'framer-motion';
 
 function FooterBar() {
   // 현재(오늘) 년도를 화면에 출력하는 상태를 설정
   const [currentYear] = useState(() => new Date().getFullYear());
-
+  
   return (
     <footer className="p-5 grid place-content-center bg-black dark:border-t dark:border-t-zinc-50/20">
       <motion.small
@@ -21,4 +21,14 @@ function FooterBar() {
   );
 }
 
-export default FooterBar;
+//React.memo(Component)
+//고차 컴포넌트 래핑
+//리렌더링에 포함되지 않음 => 두 번째 인자값 주면 그 값이 변경될 때에만 리렌더링
+export default memo(FooterBar);
+
+/* 
+컴포넌트 함수
+React.memo 고차 함수
+MemoizedComponent 고차 컴포넌트
+향상된 컴포넌트 : props가 변경되지 않은 
+*/
